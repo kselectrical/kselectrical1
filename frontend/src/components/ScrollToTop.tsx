b84e0cc77fa5ee9270544d8b9ac8b11e8c9eed06
Local Commit <local@example.com>
2026-09-04 20:48:02 +0530

    feat(analytics): add SPA route change pageview tracking to ScrollToTop component

--- frontend/src/components/ScrollToTop.tsx
+++ frontend/src/components/ScrollToTop.tsx
@@ -1,12 +1,24 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    gtag?: (...args: any[]) => void;
+  }
+}
+
 export const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname]);
+    if (typeof window.gtag === 'function') {
+      window.gtag('config', 'G-ZR2NN7MKH3', {
+        page_path: pathname + search,
+      });
+    }
+  }, [pathname, search]);
 
   return null;
 };
